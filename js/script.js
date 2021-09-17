20eabfa51ada86768854e977e374cfeb1aacae9d
FuelDoks <fueldoks@icloud.com>
2021-09-17 20:37:45 +0300

    Переменные и строгая типизация

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log(1);
+"use strict";
+
+// a = 15;          Старый формат записи и присваивания переменной
+// console.log(a);  Который не используется под современный стандарт.
+
+
+// console.log(1);
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+
+obj.a = 10;
+
+// console.log(obj);  Устаревший формат задания переменных 
+// var name = 'Name';   который не желательно использовать.
+
+{
+    let result = 50;
+}
+
+console.log(result);
