6e322642b308e93d0b41519fc4f54b1d5e6fe822
FuelDoks <fueldoks@icloud.com>
2021-10-18 21:45:23 +0300

    Объекты и типы данных

--- js/script.js
+++ js/script.js
@@ -1,28 +1,58 @@
 "use strict";
 
-// a = 15;          Старый формат записи и присваивания переменной
-// console.log(a);  Который не используется под современный стандарт.
+// // a = 15;          Старый формат записи и присваивания переменной
+// // console.log(a);  Который не используется под современный стандарт.
 
 
-// console.log(1);
-let number = 5;
-const leftBorderWidth = 1;
+// // console.log(1);
+// let number = 5;
+// const leftBorderWidth = 1;
 
-number = 10;
-console.log(number);
-const obj = {
-  a: 50
-};
+// number = 10;
+// console.log(number);
+// const obj = {
+//   a: 50
+// };
 
 
-obj.a = 10;
+// obj.a = 10;
 
-console.log(obj);
-//   Устаревший формат задания переменных
-// var name = 'Name';   который не желательно использовать.
+// console.log(obj);
+// //   Устаревший формат задания переменных
+// // var name = 'Name';   который не желательно использовать.
 
-{
-  let result = 50;
-}
+// {
+//   let result = 50;
+// }
 
-console.log(result);
+// // console.log(result);
+
+
+// let number = 4.6;
+
+console.log(4 / 0); //js позволяет совершать такие ошибки,нужно быть внимательней
+console.log('string' * 9);
+
+const person = 'Alex';
+
+
+
+// const bool = false;
+
+// console.log(something); // null
+
+// let und;
+// console.log(und); //undefined
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMaried: false
+// }
+
+// console.log(obj.name);  // обращение к обекту через значение свойства name
+// console.log(obj["name"]);  // тоже может работать но лучше через точку.
+
+
+// let arr = ['plum.png', 'orange.png', 6, 'orange.png', {}, []];
+// console.log(arr[1]);
